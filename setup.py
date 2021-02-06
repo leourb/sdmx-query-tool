e76551d791327fcce9f4c5d36219cc02ad372e3c
Leonardo Urbano <urbanstyle_87@hotmail.it>
2021-02-06 15:04:30 +0000

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sdmx_query_tool",
+    version="1.0.0",
+    author="Leonardo Urbano",
+    author_email="leonardo.urbano87@libero.it",
+    packages=setuptools.find_packages(),
+    description="SDMX client for Python users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/leourb/sdmx-query-tool",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)

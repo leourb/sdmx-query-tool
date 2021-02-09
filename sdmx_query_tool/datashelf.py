0ed18e644ee7efc733df2ce00d22d39b3ef726ba
Leonardo Urbano <urbanstyle_87@hotmail.it>
2021-02-09 10:47:57 +0000

    Added IMF configuration

--- sdmx_query_tool/datashelf.py
+++ sdmx_query_tool/datashelf.py
@@ -20,6 +20,10 @@ ECB = {
     }
 }
 
+IMF = {
+    "dsd_list": "https://registry.sdmx.org/ws/public/sdmxapi/rest/dataflow/"
+}
+
 
 class DataShelf:
     """Container for static data"""
@@ -27,11 +31,20 @@ class DataShelf:
     def __init__(self):
         """Initialize the class"""
         self.__ecb = ECB
+        self.__imf = IMF
 
     def ecb(self):
         """
         Return the ECB dictionary publicly
-        :return: a dictionary with the static ECB information
+        :return: a dictionary with the ECB configuration
         :rtype: dict
         """
         return self.__ecb
+
+    def imf(self):
+        """
+        Return the IMF dictionary publicly
+        :return: a dictionary with the IMF configuration
+        :rtype: dict
+        """
+        return self.__imf
